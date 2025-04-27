4245db14695cd6f20301d1c423c2f78599bcfadb
waxz <wz67766@163.com>
2025-04-27 18:57:16 +0800

    add cache

--- functions/index.js
+++ functions/index.js
@@ -35,6 +35,40 @@
     }
   }
   
+  export async function fetch_cache(request, ctx) {
+    const cacheUrl = new URL(request.url);
+
+    // Construct the cache key from the cache URL
+    const cacheKey = new Request(cacheUrl.toString(), request);
+    const cache = caches.default;
+
+    // Check whether the value is already available in the cache
+    // if not, you will need to fetch it from origin, and store it in the cache
+    let response = await cache.match(cacheKey);
+
+    if (!response) {
+      console.log(
+        `Response for request url: ${request.url} not present in cache. Fetching and caching request.`,
+      );
+      // If not in cache, get it from origin
+      response = await fetch(request);
+
+      // Must use Response constructor to inherit all of response's fields
+      response = new Response(response.body, response);
+
+      // Cache API respects Cache-Control headers. Setting s-max-age to 10
+      // will limit the response to be in cache for 10 seconds max
+
+      // Any changes made to the response here will be reflected in the cached value
+      response.headers.append("Cache-Control", "s-maxage=10");
+
+      ctx.waitUntil(cache.put(cacheKey, response.clone()));
+    } else {
+      console.log(`Cache hit for: ${request.url}.`);
+    }
+    return response;
+  }
+
   export async function onRequest(context) {
     const { request } = context;
     const url = new URL(request.url)
@@ -151,15 +185,8 @@
   
     try {
       // Send request to target server
-      let response = await fetch(
-        newRequest, {
-            method: request.method,
-            headers: newHeaders,
-            body: request.method !== 'GET' && request.method !== 'HEAD' ? request.body : null,
-            redirect: 'manual', // Handle redirects manually
-          }
-        
-        )
+    //   let response = await fetch(newRequest );
+      let response =  await fetch_cache(newRequest, context);
       
       // Handle redirects
       let newRespHeaders = new Headers(response.headers)
